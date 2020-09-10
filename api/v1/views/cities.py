70d44b82b2a866020f7161738aa75fea2df4cb7d
Camilo Moncada <monk-da@hotmail.com>
2020-09-10 00:43:38 -0500

    hotfix 3 task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,12 +14,12 @@ def list_citys_of_a_states(state_id=None):
     city_list = []
     state_obj = storage.get(State, state_id)
 
-    if state_obj is None:
+    if state_obj:
+        for obj in state_obj.cities:
+            city_list.append(obj.to_dict())
+        return jsonify(city_list)
+    else:
         abort(404)
-    for obj in state_obj.cities:
-        city_list.append(obj.to_dict())
-
-    return jsonify(city_list)
 
 
 @app_views.route('/cities/<city_id>', strict_slashes=False, methods=['GET'])
@@ -28,9 +28,10 @@ def list_a_city(city_id=None):
     if city_id:
         city_obj = storage.get(City, city_id)
 
-    if city_obj is None:
+    if city_obj:
+        return jsonify(city_obj.to_dict())
+    else:
         abort(404)
-    return jsonify(city_obj.to_dict())
 
 
 @app_views.route('/cities/<city_id>', strict_slashes=False, methods=['DELETE'])
@@ -39,12 +40,12 @@ def del_a_city(city_id=None):
     if city_id:
         city_obj = storage.get(City, city_id)
 
-    if city_obj is None:
-        abort(404)
-    else:
+    if city_obj:
         storage.delete(city_obj)
         storage.save()
         return (jsonify({})), 200
+    else:
+        abort(404)
 
 
 @app_views.route("/states/<state_id>/cities", methods=["POST"],
@@ -52,6 +53,10 @@ def del_a_city(city_id=None):
 def create_a_city(state_id=None):
     """create a city"""
     if state_id is None:
+        abort(404)
+
+    get_state = storage.get(State, state_id)
+    if get_state is None:
         abort(404)
 
     data = request.get_json(silent=True)
